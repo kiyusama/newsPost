0f3f69946194b33a05aa2c0ae0d1408061618e5f
kiyusama <kiyusama328@gmail.com>
2024-03-18 22:43:00 +0900

    after fixed delete

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-railsimport "@hotwired/turbo-rails"
+import "controllers"
+import "@hotwired/turbo-rails";
