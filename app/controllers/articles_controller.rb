8a4a946f9a9f9cc75a0d934ee56111abb1b4640a
kiyusama <kiyusama328@gmail.com>
2024-03-18 22:06:17 +0900

    before fix delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,52 @@
 class ArticlesController < ApplicationController
+
+
+  before_action :authenticate_user!
+
+
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: "記事が正常に投稿されました"
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article, notice: "記事が正常に更新されました"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path, notice: "記事が正常に削除されました"
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body, :genre)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   root "pages#index"
+
+  resources :articles
 end
